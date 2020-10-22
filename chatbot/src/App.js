4bf56df402ed8c06e395d21012d63d356202b59d
Gabriel Schlam <31284107+gabschlam@users.noreply.github.com>
2020-10-22 09:09:43 -0500

    Update App.js

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -5,7 +5,6 @@ import NavBar from "./Components/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chatbot from "./Components/Chatbot";
 import "react-chat-widget/lib/styles.css";
-import PrivateRoute from "./Components/PrivateRoute";
 import WhatsApp from "./Pages/WhatsApp";
 import FormPage from "./Pages/Login";
 export default function App() {
